40837ed33fa6f57f579810a7aa17aa7589a624b9
LeslieAmaya <leslieamayal001@gmail.com>
2024-10-21 14:44:44 -0700

    Agregar archivos de proyecto.

---
+++ MVVM_implementacion_JAGS/VistaModelo/VMpatron.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM_implementacion_JAGS.VistaModelo
+{
+     class VMpatron : BaseViewModel 
+    {
+        #region Variables
+        string _Texto;
+        #endregion
+
+        #region Constructor
+        #endregion
+
+        #region Objetos
+        public string Texto
+        {
+            get { return _Texto; }
+            set { SetValue(ref _Texto, value); }
+        }
+        #endregion
+
+        #region Procesos
+        public async Task ProcesoAsyncrong()
+        {
+
+        }
+
+        public void ProcesoSimple()
+        {
+
+        }
+        #endregion
+
+    }
+}
